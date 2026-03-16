d58f536805f932978839e9e082f8a60417ce1ce4
bruno e natan <brunorauldelafuentebrizuela@gmail.com>
2026-03-16 08:07:00 -0300

    Criação de classe Livro

---
+++ src/Livro.java
@@ -0,0 +1,18 @@
+import java.time.LocalTime;
+
+public class Livro {
+    //para criar uma variavel colocamos
+    //tipo nomeVariavel;
+    //double é uma variavel que armazena numeros reais ou unidades fracionarias.
+    //no java, tudo q vamos criar sao classes.
+    // para criar um objeto, criamos uma classe. E apartir da classe, criamos um objeto.
+    // criar novo objeto Ex: new Aluno();
+    String titulo;
+    String autor;
+    String narrador;
+    String editora;
+    String versao;
+    double preco;
+    String resumo;
+    LocalTime duracao;
+}
